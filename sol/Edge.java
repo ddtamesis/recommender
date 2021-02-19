c7d2274d203c7732d4c87bb981fdc060a8a0024c
Jenny Wei <jenny_wei@brown.edu>
2021-02-18 22:19:31 -0500

    Edge update

--- sol/Edge.java
+++ sol/Edge.java
@@ -1,9 +1,13 @@
 package sol;
 
+import src.INode;
+
 public class Edge {
     String value;
+    INode nextNode;
 
-    public Edge(String value) {
+    public Edge(String value, INode nextNode) {
         this.value = value;
+        this.nextNode = nextNode;
     }
 }
